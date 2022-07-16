f1d05a42a91f1ece725cfb39404c31335c2f4443
IdiosGame <36539661+HuySan@users.noreply.github.com>
2022-07-16 06:13:08 +0300

    Cherry pick fixes

---
+++ Assets/Scripts/Gameplay/Components/Player/PlayerShootingComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+namespace Gameplay.Components
+{
+    [Serializable]
+    public struct PlayerShootingComponent
+    {
+        public LayerMask maskDetecting;
+        public float radiusImpact;
+        public int hits;
+        public List<GameObject> currentHitObjects;
+    }
+}
---
+++ Assets/Scripts/Gameplay/Providers/PlayerShootingProvider.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Voody.UniLeo;
+using Gameplay.Components;
+
+namespace Gameplay.Providers
+{
+    public class PlayerShootingProvider : MonoProvider<PlayerShootingComponent> { }
+}
