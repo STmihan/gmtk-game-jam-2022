67b160bf2d923c0ee3b28a19dc892754d4a4105c
STmihan <mih.dunaev@gmail.com>
2022-07-17 13:17:41 +0300

    Some saves
    (cherry picked from commit b832054ced52cd9fa136feacfe53ebcba877e66c)

---
+++ Assets/Scripts/Gameplay/Components/Tile/TileComponent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Components.Tile
+{
+    public struct TileComponent
+    {
+        public Vector3 Up;
+        public Vector3 Down;
+        public Vector3 Left;
+        public Vector3 Right;
+    }
+}
---
+++ Assets/Scripts/Gameplay/Systems/Tile/TileSetterSystem.cs
@@ -0,0 +1,20 @@
+﻿using Gameplay.Configs;
+using Leopotam.Ecs;
+
+namespace Gameplay.Systems.Tile
+{
+    public class TileSetterSystem : IEcsRunSystem, IEcsInitSystem
+    {
+        private EcsWorld _world;
+        private TilesConfig _config;
+        public void Init()
+        {
+            
+        }
+
+        public void Run()
+        {
+            
+        }
+    }
+}
